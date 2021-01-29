f1c09ef57cb418217c4867e97fb3dc1e572ffbe5
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-29 22:42:29 +0100

    - added camera follow and player movement scripts

---
+++ Global Game Jam 2021/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+  
+    void Update()
+    {
+        transform.position = new Vector3 (player.transform.position.x, transform.position.y, player.transform.position.z);
+    }
+}
---
+++ Global Game Jam 2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+}
