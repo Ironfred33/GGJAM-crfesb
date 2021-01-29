d98bfbab3244ea4b8cab4a7c198731f6bdd5853e
Chris Reimelt <chris.reimelt@gmail.com>
2021-01-30 00:48:48 +0100

    pls help

--- Global Game Jam 2021/Assets/Scripts/PlayerMovement.cs
+++ Global Game Jam 2021/Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-
     private Vector3 moveInput;
     private Vector3 moveVelocity;
     private Rigidbody rb;
+
+    public float speed = 4;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
+        //moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveVelocity = moveInput * moveSpeed;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = moveVelocity;
+        //rb.velocity = moveVelocity;
     }
 }
---
+++ Global Game Jam 2021/Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector3 mousePos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("Input Mouse X: " + Input.mousePosition.x);
+        Debug.Log("Input Mouse Y: " + Input.mousePosition.y);
+
+        //Get the Screen positions of the object
+         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+         
+         //Get the Screen position of the mouse
+         Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+         
+         //Get the angle between the points
+         float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+ 
+         //Ta Daaa
+         transform.rotation = Quaternion.Euler(new Vector3(0f,-angle + 270, 0f));
+          
+        
+    }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+}
