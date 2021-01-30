63a7b344e6b5fabef4d5f12c253f9e794da0af5a
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-30 12:49:14 +0100

    - ball physics geändert

---
+++ Global Game Jam 2021/Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ballState
+{
+    blue, 
+    yellow,
+    red
+}
+
+public class Ball : MonoBehaviour
+{
+    public ballState currentState;
+    private Light ballLight;
+    private MeshRenderer ballRenderer;
+
+    void Start()
+    {
+        ballLight = GetComponent<Light>();
+        ballRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void RedBall()
+    {
+        ballLight.color = Color.red;
+        ballRenderer.material.SetColor("_EmissionColor", Color.red);
+          
+    }
+
+
+    void Update()
+    {
+        if(currentState == ballState.red)
+        {
+            RedBall();
+        }
+    }
+}
