7fcbc58decca9ab6cc5c0bd11ccee184307feb60
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-30 10:35:40 +0100

    -reworked particle system - added SceneLoader

---
+++ Global Game Jam 2021/Assets/Scripts/ParticleFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollow : MonoBehaviour
+{
+    public Transform ballPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(ballPos.position.x, ballPos.position.y, ballPos.position.z);
+    }
+}
---
+++ Global Game Jam 2021/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    // Load next level
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Load first level
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadEndScreen()
+    {
+        SceneManager.LoadScene("EndScreen");
+    }
+}
