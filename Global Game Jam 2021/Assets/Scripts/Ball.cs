55a72653b4b88e644d7226d8d67bcb633f3f8147
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-30 17:41:29 +0100

    - State Machine Anpassungen

--- Global Game Jam 2021/Assets/Scripts/Ball.cs
+++ Global Game Jam 2021/Assets/Scripts/Ball.cs
@@ -14,18 +14,33 @@ public class Ball : MonoBehaviour
     public ballState currentState;
     private Light ballLight;
     private MeshRenderer ballRenderer;
+    public ParticleSystem ps;
+
+    private Gradient grad;
 
     void Start()
     {
         ballLight = GetComponent<Light>();
         ballRenderer = GetComponent<MeshRenderer>();
+        var col = ps.colorOverLifetime;
+        grad = new Gradient();
     }
 
     private void RedBall()
     {
         ballLight.color = Color.red;
         ballRenderer.material.SetColor("_EmissionColor", Color.red);
-          
+
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.red, 0.5f), new GradientColorKey(Color.red, 1f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 0.7f), new GradientAlphaKey(0.0f, 1.0f) });
+
+        // col.color = grad;
+    }
+
+    private void BlueBall()
+    {
+        ballLight.color = Color.blue;
+        ballRenderer.material.SetColor("_EmissionColor", Color.blue);
     }
 
 
@@ -34,6 +49,11 @@ public class Ball : MonoBehaviour
         if(currentState == ballState.red)
         {
             RedBall();
+        }
+
+        else if(currentState == ballState.blue)
+        {
+            BlueBall();
         }
     }
 }
