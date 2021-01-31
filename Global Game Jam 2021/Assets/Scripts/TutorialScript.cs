2a5d841a4a9caba6f28401b0646335ab6d9d61ef
Frederik Exner <frederik.exner@uni-bayreuth.de>
2021-01-31 15:56:03 +0100

    - added tutorial

---
+++ Global Game Jam 2021/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject tutText;
+
+    void Start()
+    {
+        StartCoroutine(displayText());
+    }
+
+
+    private IEnumerator displayText()
+    {
+        tutText.SetActive(true);
+
+        yield return new WaitForSeconds(5);
+
+        tutText.SetActive(false);
+    }
+}
