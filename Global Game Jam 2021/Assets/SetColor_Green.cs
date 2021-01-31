e97dc5ff717f453a15cb0dc8d885afda4a230b1e
Chris Reimelt <chris.reimelt@gmail.com>
2021-01-31 15:37:30 +0100

    grünes Tor Prefab

---
+++ Global Game Jam 2021/Assets/SetColor_Green.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor_Green : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sphereColor;
+
+    void Start()
+    {
+
+        Light light = sphereColor.GetComponent<Light>();
+        Renderer rend = sphereColor.GetComponent<Renderer>();
+        
+        
+        light.color = Color.green;
+        rend.material.SetColor("_Color", Color.green);
+        
+    }
+}
---
+++ Global Game Jam 2021/Assets/Tor_Green_Trigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tor_Green_Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Ball ballMachine;
+    
+     void OnTriggerEnter(Collider other) {
+
+           
+    if (other.gameObject.CompareTag("Sphere")  && ballMachine.currentState == ballState.green) 
+            
+    {
+    Destroy(this.gameObject);
+
+    Destroy(other.gameObject);
+                 
+    }
+}
+}
